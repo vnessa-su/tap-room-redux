a0102580ca84c843762790eb3f6d8fc5db2fb427
Vanessa Su <vnessa.su@gmail.com>
2021-07-02 19:35:36 -0400

    :truck: Move App files to component folder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header/Header";
+import KegContent from "./Kegs/KegContent";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <KegContent />
+    </div>
+  );
+}
+
+export default App;

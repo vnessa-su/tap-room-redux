4e3003c5125606518a43424e73a0e4d31209b838
Vanessa Su <vnessa.su@gmail.com>
2021-07-05 23:14:35 -0400

    :hammer: Add edit button and form functionality

--- src/components/Kegs/KegContent.js
+++ src/components/Kegs/KegContent.js
@@ -4,12 +4,14 @@ import NewKegForm from "./NewKegForm";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import KegDetails from "./KegDetails";
+import EditKegForm from "./EditKegForm";
 
 class KegContent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showForm: false,
+      showEditForm: false,
       selectedKeg: null,
       itemList: [],
     };
@@ -19,6 +21,7 @@ class KegContent extends React.Component {
     if (this.state.selectedKeg != null) {
       this.setState({
         showForm: false,
+        showEditForm: false,
         selectedKeg: null,
       });
     } else {
@@ -50,26 +53,53 @@ class KegContent extends React.Component {
     this.setState({ itemList: [...itemListWithoutSelected, kegSelected] });
   };
 
+  onSaveKegEditHandler = (keg) => {
+    const itemListWithoutSelected = this.state.itemList.filter(
+      (item) => item.id !== keg.id
+    );
+    this.setState({
+      showForm: false,
+      showEditForm: false,
+      selectedKeg: null,
+      itemList: [...itemListWithoutSelected, keg],
+    });
+  };
+
+  onEditKegClickHandler = (id) => {
+    const kegSelected = this.state.itemList.find((keg) => keg.id === id);
+    this.setState({
+      showEditForm: true,
+      selectedKeg: kegSelected,
+    });
+  };
+
   render() {
     let visibleContent = null;
-    let buttonText = null;
-    if (this.state.selectedKeg != null) {
+    let buttonText = "Return to Keg List";
+    if (this.state.showEditForm) {
+      visibleContent = (
+        <EditKegForm
+          keg={this.state.selectedKeg}
+          onSave={this.onSaveKegEditHandler}
+        />
+      );
+    } else if (this.state.selectedKeg != null) {
       visibleContent = (
         <KegDetails
           keg={this.state.selectedKeg}
           onSellPint={this.onDecreasePintsHandler}
+          onClickEdit={this.onEditKegClickHandler}
         />
       );
-      buttonText = "Return to Event List";
     } else if (this.state.showForm) {
       visibleContent = <NewKegForm onSubmit={this.onSubmitNewKegHandler} />;
-      buttonText = "Return to Keg List";
     } else {
       visibleContent = (
         <KegList
           items={this.state.itemList}
           kegSelectHandler={this.onKegSelect}
           onSellPint={this.onDecreasePintsHandler}
+          onClickEdit={this.onEditKegClickHandler}
         />
       );
       buttonText = "Add New Keg";

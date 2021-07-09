120fe5f3e29682ffdde598efca0aa4314d60591d
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 17:57:33 -0400

    :test_tube: Add test for selectKegReducer clear select action

--- src/__tests__/reducers/select-keg-reducer.test.js
+++ src/__tests__/reducers/select-keg-reducer.test.js
@@ -14,11 +14,18 @@ describe("selectKegReducer", () => {
     expect(selectKegReducer({}, { type: null })).toEqual({});
   });
 
-  it("should return an object of the input keg data", () => {
+  it("should return an object of the input keg data for change select action", () => {
     const inputAction = {
       type: "CHANGE_KEG_SELECT",
       ...inputKegData,
     };
     expect(selectKegReducer({}, inputAction)).toEqual(inputKegData);
+  });
+
+  it("should return an empty object for clear select action", () => {
+    const inputAction = {
+      type: "CLEAR_KEG_SELECT",
+    };
+    expect(selectKegReducer(inputKegData, inputAction)).toEqual({});
   });
 });

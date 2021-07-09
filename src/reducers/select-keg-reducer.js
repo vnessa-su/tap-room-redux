4c980d3955e7f990d9d51f69738156e9ed089f34
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 17:49:13 -0400

    :white_check_mark: Pass selectKegReducer change keg select test

--- src/reducers/select-keg-reducer.js
+++ src/reducers/select-keg-reducer.js
@@ -1,5 +1,20 @@
 const selectKegReducer = (state = {}, action) => {
-  return state;
+  const { name, brand, alcoholContent, price, pintsLeft, id } = action;
+  let newState;
+  switch (action.type) {
+    case "CHANGE_KEG_SELECT":
+      newState = {
+        name: name,
+        brand: brand,
+        alcoholContent: alcoholContent,
+        price: price,
+        pintsLeft: pintsLeft,
+        id: id,
+      };
+      return newState;
+    default:
+      return state;
+  }
 };
 
 export default selectKegReducer;

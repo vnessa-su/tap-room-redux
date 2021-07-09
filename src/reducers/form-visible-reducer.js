2068aa0021eb66b044ad1c316380c362b588dbbe
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 14:36:34 -0400

    :white_check_mark: Pass set formVisibilityReducer new property to false test

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,8 +1,13 @@
 const formVisibleReducer = (state = {}, action) => {
+  let newState;
   switch (action.type) {
     case "TURN_ON_NEW":
-      let newState = { ...state };
+      newState = { ...state };
       newState.new = true;
+      return newState;
+    case "TURN_OFF_NEW":
+      newState = { ...state };
+      newState.new = false;
       return newState;
     default:
       return state;

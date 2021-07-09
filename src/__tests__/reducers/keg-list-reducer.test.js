fb52c5874a2f8d7e8529d69acea126d505477536
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 12:11:29 -0400

    :test_tube: Add test to add keg to empty keg list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,42 @@
-import kegListReducer from '../../reducers/keg-list-reducer';
+import kegListReducer from "../../reducers/keg-list-reducer";
 
-describe('kegListReducer', () => {
-  it('should return the default state', () => {
-    expect(kegListReducer()).toEqual({})
+describe("kegListReducer", () => {
+  const inputKegData = {
+    name: "Boom Sauce",
+    brand: "Lord Hobo",
+    alcoholContent: 8,
+    price: 3.5,
+    pintsLeft: 20,
+    id: 1,
+  };
+
+  it("should return the default state", () => {
+    expect(kegListReducer()).toEqual({});
   });
-})
+
+  it("should return an object contaning the input keg data", () => {
+    const { name, brand, alcoholContent, price, pintsLeft, id } = inputKegData;
+    const inputAction = {
+      type: "ADD_KEG",
+      name: name,
+      brand: brand,
+      alcoholContent: alcoholContent,
+      price: price,
+      pintsLeft: pintsLeft,
+      id: id,
+    };
+
+    const expectedOutput = {
+      [id]: {
+        name: name,
+        brand: brand,
+        alcoholContent: alcoholContent,
+        price: price,
+        pintsLeft: pintsLeft,
+        id: id,
+      },
+    };
+
+    expect(kegListReducer({}, inputAction)).toEqual(expectedOutput);
+  });
+});

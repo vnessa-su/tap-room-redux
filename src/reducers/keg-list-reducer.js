9e5df88fa66102e8fcc02e37003313b4165b5053
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 13:31:11 -0400

    :white_check_mark: Pass delete keg from keg list test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -12,6 +12,10 @@ const kegListReducer = (state = {}, action) => {
           id: id,
         },
       });
+    case "DELETE_KEG":
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
     default:
       return {};
   }

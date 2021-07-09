fd47ebac565c7dc71adb00fd64cdcdb844237eac
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 17:48:49 -0400

    :test_tube: Add test for selectKegReducer change keg select

--- src/__tests__/reducers/select-keg-reducer.test.js
+++ src/__tests__/reducers/select-keg-reducer.test.js
@@ -1,7 +1,24 @@
 import selectKegReducer from "./../../reducers/select-keg-reducer";
 
 describe("selectKegReducer", () => {
+  const inputKegData = {
+    name: "Boom Sauce",
+    brand: "Lord Hobo",
+    alcoholContent: 8,
+    price: 3.5,
+    pintsLeft: 20,
+    id: 1,
+  };
+
   it("should return default state if no action specified", () => {
     expect(selectKegReducer({}, { type: null })).toEqual({});
+  });
+
+  it("should return an object of the input keg data", () => {
+    const inputAction = {
+      type: "CHANGE_KEG_SELECT",
+      ...inputKegData,
+    };
+    expect(selectKegReducer({}, inputAction)).toEqual(inputKegData);
   });
 });

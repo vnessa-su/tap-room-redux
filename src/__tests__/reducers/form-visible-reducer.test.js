b3c5af8108d3d06a11221cd696885191991bac51
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 14:19:02 -0400

    :test_tube: Add set formVisibleReducer new property to true test

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,7 +1,23 @@
 import formVisibleReducer from "./../../reducers/form-visible-reducer";
 
 describe("formVisibleReducer", () => {
+  const initialFormVisibility = {
+    new: false,
+    edit: false,
+  };
+
   it("should return default state if no action specified", () => {
     expect(formVisibleReducer({}, { type: null })).toEqual({});
+  });
+
+  it("should return object with new property set to true", () => {
+    const inputAction = { type: "TURN_ON_NEW" };
+    const expectedOutput = {
+      new: true,
+      edit: false,
+    };
+    expect(formVisibleReducer(initialFormVisibility, inputAction)).toEqual(
+      expectedOutput
+    );
   });
 });

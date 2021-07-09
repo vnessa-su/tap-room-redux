88a4832003dd01bc7cd0bc381637ce14ad53095f
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 12:23:41 -0400

    :white_check_mark: Pass add keg to empty keg list test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,5 +1,20 @@
-const kegListReducer = (state={}, action) => {
-  return {};
-}
+const kegListReducer = (state = {}, action) => {
+  const { name, brand, alcoholContent, price, pintsLeft, id } = action;
+  switch (action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          alcoholContent: alcoholContent,
+          price: price,
+          pintsLeft: pintsLeft,
+          id: id,
+        },
+      });
+    default:
+      return {};
+  }
+};
 
 export default kegListReducer;

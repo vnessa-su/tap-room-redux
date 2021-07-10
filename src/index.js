10602b7ed0fe55789094e9269cabc4d9e8974f7e
Vanessa Su <vnessa.su@gmail.com>
2021-07-09 20:19:21 -0400

    :hammer: Add store to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./reducers/index";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
